7ac86f1bdf9dc4aa94acc73f39b9ef23b5004d9e
Hugo David Farji <hdf1986.6700@gmail.com>
2017-04-10 22:26:42 -0300

    Updated dot-files!

--- open-tmux.sh
+++ open-tmux.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 tmux new-session -d
-cd ~/.dot-files
+# cd ~/.dot-files
 #git fetch origin > /dev/null 2>&1
 #reslog=$(git log HEAD..origin/master --oneline)
 #if [[ "${reslog}" != "" ]] ; then
 #  tmux split-window -d 'echo "There is updates for your dot-files folder. You can pull them using pull-my-fucking-dot-files :)"; read'
 #  tmux resize-pane -t 1 -y 3
 #fi
-cd -
+# cd -
 # Just for a near future...
 # tmux-next split-window -d 'wit'
 # tmux-next resize-pane -t 1 -y 3

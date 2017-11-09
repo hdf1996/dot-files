b23bcd2535e5d1faed3db4b313a74b9d6e9aa52b
Travis CI <hdf1986.6700@gmail.com>
2017-11-09 10:07:22 -0300

    Updated dot-files!

---
+++ window-manager/keybindings/multimedia.sh
@@ -0,0 +1,3 @@
+hc keybind XF86AudioRaiseVolume spawn amixer -q set Master 5%+ unmute
+hc keybind XF86AudioLowerVolume spawn amixer -q set Master 5%- unmute
+hc keybind XF86AudioMute spawn $HOME/.dot-files/bin/mute

04c03549b124ba237cbe30fbb551255a82191f9d
Hugo David Farji <hdf1986.6700@gmail.com>
2018-06-06 20:05:30 -0400

    Updated dot-files!

--- .hyper.js
+++ .hyper.js
@@ -34,84 +34,43 @@ module.exports = {
     // custom padding (css format, i.e.: `top right bottom left`)
     padding: '12px 14px',
 
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
-    colors: {
-            black: '#000000',
-            red: '#ff0000',
-            green: '#33ff00',
-            yellow: '#ffff00',
-            blue: '#0066ff',
-            magenta: '#cc00ff',
-            cyan: '#00ffff',
-            white: '#d0d0d0',
-            lightBlack: '#808080',
-            lightRed: '#ff0000',
-            lightGreen: '#33ff00',
-            lightYellow: '#ffff00',
-            lightBlue: '#0066ff',
-            lightMagenta: '#cc00ff',
-            lightCyan: '#00ffff',
-            lightWhite: '#ffffff',
-          },
-
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    //
-    // Windows
-    // - Make sure to use a full path if the binary name doesn't work
-    // - Remove `--login` in shellArgs
-    //
-    // Bash on Windows
-    // - Example: `C:\\Windows\\System32\\bash.exe`
-    //
-    // Powershell on Windows
-    // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
     shell: '',
 
-    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
-    // by default ['--login'] will be used
     shellArgs: ['--login'],
 
-    // for environment variables
     env: {},
-
-    // set to false for no bell
     bell: 'SOUND',
 
-    // if true, selected text will automatically be copied to the clipboard
     copyOnSelect: true
-
-    // if true, on right click selected text will be copied or pasted if no
-    // selection is present (true by default on Windows)
-    // quickEdit: true
-
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
-
     // for advanced config flags please refer to https://hyper.is/#cfg
   },
 
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hyperpower`
-  //   `@company/project`
-  //   `project#1.0.1`
   plugins: [
     'hyperline',
-    'hyper-snazzy',
     'hyper-keymap',
-    'hyper-material-theme',
+    'verminal',
+    'hyperterm-overlay',
+    'hyperminimal'
   ],
+    overlay: {
+      alwaysOnTop: true,
+      animate: true,
+      hasShadow: false,
+      hideDock: false,
+      hideOnBlur: true, 
+      hotkeys: ['Ctrl+Space'],
+      position: 'top',
+      primaryDisplay: false,
+      resizable: true,
+      startAlone: false,
+      startup: false,
+      size: 0.4,
+      tray: true,
+      unique: false
+    },
 
-  // in development, you can create a directory under
-  // `~/.hyper_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
   localPlugins: [],
   keymaps: {
-    // Example
     // 'window:devtools': 'cmd+alt+o',
   }
 };
